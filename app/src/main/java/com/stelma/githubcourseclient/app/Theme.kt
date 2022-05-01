03aff372b2dfdb28d2e65c953de190cdd71e64df
vovastelmashchuk <vovochkastelmashchuk@gmail.com>
2022-05-01 16:42:00 +0300

    Fixed theme, add app compact, add baseline share icon.

--- app/src/main/java/com/stelma/githubcourseclient/app/Theme.kt
+++ app/src/main/java/com/stelma/githubcourseclient/app/Theme.kt
@@ -1,15 +1,13 @@
 package com.stelma.githubcourseclient.app
 
-import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.Colors
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Shapes
 import androidx.compose.material.Typography
-import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
-private val DarkColorPalette = darkColors(
+private val ColorPalette = Colors(
     primary = Color(0xFF2D6503),
     primaryVariant = Color(0xFF8DC463),
     onPrimary = Color(0xFFFFFFFF),
@@ -22,35 +20,15 @@ private val DarkColorPalette = darkColors(
     onSurface = Color(0xFFFFFFFF),
     error = Color(0xFFB00020),
     onError = Color(0xFFFFFFFF),
-)
-
-private val LightColorPalette = lightColors(
-    primary = Color(0xFF2D6503),
-    primaryVariant = Color(0xFF8DC463),
-    onPrimary = Color(0xFFFFFFFF),
-    secondary = Color(0xFF78DA70),
-    secondaryVariant = Color(0xFF007812),
-    onSecondary = Color(0xFF000000),
-    background = Color(0xFFFFFFFF),
-    onBackground = Color(0xFF000000),
-    surface = Color(0xFFFFFFFF),
-    onSurface = Color(0xFF000000),
-    error = Color(0xFFB00020),
-    onError = Color(0xFFFFFFFF),
+    isLight = false,
 )
 
 @Composable
-fun GithubAppTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-  val colors = if (darkTheme) {
-    DarkColorPalette
-  } else {
-    LightColorPalette
-  }
-
+fun GithubAppTheme(content: @Composable () -> Unit) {
   MaterialTheme(
-      colors = colors,
+      colors = ColorPalette,
       typography = Typography(),
       shapes = Shapes(),
-      content = content
+      content = content,
   )
 }
