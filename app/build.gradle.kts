bafc60bf15cbcfd3c43330f45d5b8fd5cdc72f72
vovastelmashchuk <vovochkastelmashchuk@gmail.com>
2022-05-04 20:47:30 +0300

    Migrate gradle to kotlin script

---
+++ app/build.gradle.kts
@@ -0,0 +1,62 @@
+plugins {
+  id("com.android.application")
+  kotlin("android")
+}
+
+val compose_version = "1.1.1"
+
+android {
+  compileSdkVersion(31)
+
+  defaultConfig {
+    applicationId = "com.stelma.GitHubCourseClient"
+    minSdkVersion(22)
+    targetSdkVersion(31)
+    versionCode = 1
+    versionName = "1.0"
+
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    vectorDrawables.useSupportLibrary = true
+  }
+
+  buildTypes {
+    release {
+      isMinifyEnabled = false
+      proguardFiles(
+          getDefaultProguardFile("proguard-android-optimize.txt"),
+      )
+    }
+  }
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+  }
+  kotlinOptions {
+    jvmTarget = "1.8"
+  }
+  buildFeatures {
+    compose = true
+  }
+  composeOptions {
+    kotlinCompilerExtensionVersion = compose_version
+  }
+  packagingOptions {
+    resources {
+      excludes += "/META-INF/{AL2.0,LGPL2.1}"
+    }
+  }
+}
+
+dependencies {
+  implementation("androidx.appcompat:appcompat:1.4.1")
+  implementation("androidx.core:core-ktx:1.7.0")
+  implementation("androidx.compose.ui:ui:$compose_version")
+  implementation("androidx.compose.material:material:$compose_version")
+  implementation("androidx.compose.ui:ui-tooling-preview:$compose_version")
+  implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.1")
+  implementation("androidx.activity:activity-compose:1.4.0")
+
+  implementation("io.coil-kt:coil-compose:2.0.0-rc03")
+
+  testImplementation("junit:junit:4.13.2")
+}
